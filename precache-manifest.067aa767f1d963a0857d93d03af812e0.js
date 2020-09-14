19b11d7685b78fd62fd08102521b7f0dff2648f4
Vitalii <vitalii.luniev83@gmail.com>
2020-09-14 18:35:42 +0300

    Updates

---
+++ precache-manifest.067aa767f1d963a0857d93d03af812e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4578c4b022f857fc023e12a4ccf107c7",
+    "url": "/React-ToDo-List/index.html"
+  },
+  {
+    "revision": "7ab204f7761d93cad359",
+    "url": "/React-ToDo-List/static/css/main.4bd498f4.chunk.css"
+  },
+  {
+    "revision": "61ec74d5e3a73cb33122",
+    "url": "/React-ToDo-List/static/js/2.97f93d4d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-ToDo-List/static/js/2.97f93d4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ab204f7761d93cad359",
+    "url": "/React-ToDo-List/static/js/main.e6a34bb9.chunk.js"
+  },
+  {
+    "revision": "120bc3174fcab45d566c",
+    "url": "/React-ToDo-List/static/js/runtime-main.07be694b.js"
+  }
+]);
